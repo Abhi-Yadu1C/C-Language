461e6a9f341485c22c7ca6a89693ce67c2a0594c
Abhishek Yadav <anuragabhi7303@gmail.com>
2024-09-18 18:55:36 +0530

    Chapter1

---
+++ Chapter 1/02_data_types.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    // data types 
+
+    // int a ;
+    // a = 1;
+    int a = 1;
+
+    // float b = 1.4;
+    float b;
+    b = 1.4;
+
+    // char c = 'a';
+    char c;
+    c = 'a';
+
+    
+    /*
+    types of constants
+
+    1. Integer Constant -> 1,5,8,9
+    2. Real Constant -> 43.33, 3.4, 
+    3. Character Constant -> 'a','A','1', '$'
+    */
+    
+}
+
---
+++ Chapter 1/03_input.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    char a;
+    a=10;
+    scanf("%d", &a);
+    printf("The value of a is %d", a);
+    return 0;
+}
