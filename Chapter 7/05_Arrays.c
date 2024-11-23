05025b6b4d571d55328a8396cac6823e6b1929f1
Abhishek Yadav <anuragabhi7303@gmail.com>
2024-11-23 23:15:37 +0530

    Arrays

---
+++ Chapter 7/05_Arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int marks[] = {12, 43, 53, 66};
+    int* ptr = &marks[0];
+    //int* ptr = marks; // Same as int* ptr = &marks[0];
+
+    for(int i = 0; i < 4; i++){
+        printf("The marks at index %d is %d\n",i, *ptr);
+        ptr++;
+    }
+    return 0;
+}
